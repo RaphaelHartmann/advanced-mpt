084161767ea7350598bee8e2b7629e2bde3ef1d6
Raphael <36821713+RaphaelHartmann@users.noreply.github.com>
2024-12-04 15:47:24 +0100

    Add files via upload

---
+++ material/exercises/yourturn/exercises.R
@@ -0,0 +1,48 @@
+
+library(rtmpt)
+
+# SOLUTIONS TO THE EXERCISES #
+
+## 1 ##
+### EQN ###
+EQN_file <- # save the EQN file path here
+
+
+## 2 ##
+### MDL ###
+MDL_file <- # save the MDL file path here
+
+
+## 3 ##
+
+
+
+## 4.1 ##
+test_model <- # set g = 0.5
+test_model <- # suppress process time of Fn and Fr in plus direction
+
+
+## 4.2 ##
+test_model <- # set rel. starting point of Ku and Kk to 0.5
+test_model <- # set drift rate for Fn and Fr equal
+
+
+## 5 ##
+### exponential ###
+  
+
+### diffusion ###
+
+
+
+## 6 ##
+### exponential ###
+e_data <- # transform the data for the exponential model
+  
+### diffusion ###
+d_data <- # transform the data for the diffusion model
+
+
+## 7 ##
+e_fit <- # fit the exponential model
+
